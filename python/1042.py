0384d8d59357fc33a4f852637a2df62654c89479
emamoura <emanuel.moura@ccc.ufcg.edu.br>
2020-05-23 17:41:20 -0300

    Solução da questão "Sort Simples"

---
+++ python/1042.py
@@ -0,0 +1,14 @@
+'''
+Sort Simples - URI Online Judge - 1042.py
+
+Emanuel Araújo de Moura
+'''
+
+valores = list(map(int, input().split()))
+saida = sorted(valores)
+
+for numero in saida:
+    print(numero)
+print()
+for numero in valores:
+    print(numero)
